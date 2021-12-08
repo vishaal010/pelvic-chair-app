70aab89ba030b7d2839eaeaa1327860247aacecd
Kimono きもの <34915099+kimono-k@users.noreply.github.com>
2021-12-08 13:23:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Pelvic Chair 2 - Dashboard</title>
+    <title>Pelvic Chair 2 Dashboard</title>
 
     <!-- Custom fonts for this template -->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -19,12 +18,12 @@
 
     <!-- Custom styles for this template-->
     <link href="../public/css/style.css" rel="stylesheet">
-
 </head>
 
 <header>
  
 </header>
+
 <body id="page-top">
    
 
