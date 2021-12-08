5dea4d684cd791141c2fd2283acb0c8c4c3d4f9e
Kimono きもの <34915099+kimono-k@users.noreply.github.com>
2021-12-08 12:54:31 +0100

    incomplete models added

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const ResultSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+});
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date_of_birth: {
+    type: Date,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  verified: {
+    type: Boolean,
+  },
+  results: [ResultSchema],
+});
+
+module.exports = mongoose.model("User", UserSchema);
